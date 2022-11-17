9b693d571636f758afc29fbc27f30d41729de35b
魏海球 <1196396546@qq.com>
2022-11-17 15:41:11 +0800

    第一次上传

---
+++ src/main/java/com/open/space/OpenSpaceApplication.java
@@ -0,0 +1,13 @@
+package com.open.space;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OpenSpaceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OpenSpaceApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/open/space/OpenSpaceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.open.space;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OpenSpaceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
