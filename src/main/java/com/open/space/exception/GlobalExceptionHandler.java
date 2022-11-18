0372ac2b21dc88f72ac3ba1f074013cfd6797dc1
魏海球 <1196396546@qq.com>
2022-11-18 10:46:57 +0800

    第二次上传

---
+++ src/main/java/com/open/space/exception/GlobalExceptionHandler.java
@@ -0,0 +1,30 @@
+package com.open.space.exception;
+
+import com.open.space.common.BaseResponse;
+import com.open.space.common.ErrorCode;
+import com.open.space.common.ResultUtils;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+/**
+ * 全局异常处理器
+ *
+ * @author whq
+ */
+@RestControllerAdvice
+@Slf4j
+public class GlobalExceptionHandler {
+
+    @ExceptionHandler(BusinessException.class)
+    public BaseResponse<?> businessExceptionHandler(BusinessException e) {
+        log.error("businessException: " + e.getMessage(), e);
+        return ResultUtils.error(e.getCode(), e.getMessage());
+    }
+
+    @ExceptionHandler(RuntimeException.class)
+    public BaseResponse<?> runtimeExceptionHandler(RuntimeException e) {
+        log.error("runtimeException", e);
+        return ResultUtils.error(ErrorCode.SYSTEM_ERROR, e.getMessage());
+    }
+}
